fd944133d27398b561ab31ad153ebfcb4f3bca9a
Gitmycode <marbouchl@gmail.com>
2014-05-01 14:56:59 -0200

    premier commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'db554c8c5c6f41567b57dbaba9c430088e30de3fcea34a5ddbd16f6a0aa6ecb392caf3686a53a85d7793534566b09abfec4f90b999b04da51785285595190665'
